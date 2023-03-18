668634f9ec1274e24867b1c7a37017b470887650
riponmollick66 <87811215+riponmollick66@users.noreply.github.com>
2023-03-18 13:56:28 +0600

    Update install_influxdb+telegraf+grafana.sh

--- install_influxdb+telegraf+grafana.sh
+++ install_influxdb+telegraf+grafana.sh
@@ -2,7 +2,8 @@
 #====#===========#========
 /usr/bin/apt update	-y ;
 /usr/bin/apt install -y curl gnupg wget	;
-# # Adding Repository
+
+# Adding Repository
 #====#===========#=====
 /usr/bin/wget -q https://repos.influxdata.com/influxdata-archive_compat.key
 echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
