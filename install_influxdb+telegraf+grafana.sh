b798d3f3db4d029abc7da5ee62301ead34a481f0
Ripon Mollick <87811215+riponmollick66@users.noreply.github.com>
2023-04-06 14:29:23 +0600

    Update install_influxdb+telegraf+grafana.sh

--- install_influxdb+telegraf+grafana.sh
+++ install_influxdb+telegraf+grafana.sh
@@ -26,29 +26,29 @@ echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https
 
 # Add user to Influxdb
 #====#===========#===========
-# influx	
-# CREATE USER "admin" WITH PASSWORD 'admin' WITH ALL PRIVILEGES
-# show users
-# exit
+ influx	
+ CREATE USER "admin" WITH PASSWORD 'admin' WITH ALL PRIVILEGES
+ show users
+ exit
 
 # Configuring Influxbd Authentication
 #=============#==============#==========
-# sudo nano /etc/influxdb/influxdb.conf
+sudo nano /etc/influxdb/influxdb.conf
 # Locate the [http] section, uncomment the auth-enabled option, and set its value to true
 
 # Configuring Telegraf
 #====#===========#===========
-# nano /etc/telegraf/telegraf.conf
+nano /etc/telegraf/telegraf.conf
 # Locate the [outputs.influxdb] section and provide the username and password
 
 
 # See telegraf storing in the database
 #=============#==============#==========
-# influx -username 'admin' -password 'admin'
-# show databases
-# use telegraf
-# show measurements
-# exit
+ influx -username 'admin' -password 'admin'
+ show databases
+ use telegraf
+ show measurements
+ exit
 
 # Restart the services
 #====#===========#===========
