e93130c5816ebab666c3276fb76945043a98736e
Ripon Mollick <87811215+riponmollick66@users.noreply.github.com>
2023-06-09 20:22:47 +0600

    Update install_prometheus.sh

--- install_prometheus.sh
+++ install_prometheus.sh
@@ -1,49 +1,22 @@
 # Required Commands
 #=====#========#=====
-sudo groupadd --system prometheus	;
-sudo useradd -s /sbin/nologin --system -g prometheus prometheus	;
-sudo mkdir /var/lib/prometheus	;
-sudo mkdir -p /etc/prometheus	;
-cd /opt	;
-/usr/bin/wget https://github.com/prometheus/prometheus/releases/download/v2.42.0/prometheus-2.42.0.linux-amd64.tar.gz	;
-tar xvf prometheus*.tar.gz	;
-cd prometheus-2.42.0.linux-amd64	;
-sudo mv prometheus promtool /usr/local/bin/	;
-sudo mv prometheus.yml /etc/prometheus/prometheus.yml	;
-sudo mv consoles/ console_libraries/ /etc/prometheus/	;
-rm -rf /opt/prometheus	;
+/usr/bin/wget https://github.com/prometheus/prometheus/releases/download/v2.44.0/prometheus-2.44.0.linux-amd64.tar.gz	;
+tar xvf prometheus-2.44.0.linux-amd64.tar.gz	;
+/bin/mv prometheus-2.44.0.linux-amd64 /etc/prometheus/
 
 # create systemd service
 #========#==========#======
 sudo tee /etc/systemd/system/prometheus.service<<EOF
 [Unit]
 Description=Prometheus
-Documentation=https://prometheus.io/docs/introduction/overview/
 Wants=network-online.target
 After=network-online.target
 [Service]
-Type=simple
-User=prometheus
-Group=prometheus
-ExecReload=/bin/kill -HUP \$MAINPID
-ExecStart=/usr/local/bin/prometheus \
-  --config.file=/etc/prometheus/prometheus.yml \
-  --storage.tsdb.path=/var/lib/prometheus \
-  --web.console.templates=/etc/prometheus/consoles \
-  --web.console.libraries=/etc/prometheus/console_libraries \
-  --web.listen-address=0.0.0.0:9090 \
-  --web.external-url=
-SyslogIdentifier=prometheus
+ExecStart=/etc/prometheus/prometheus --config.file=/etc/prometheus/prometheus.yml
 Restart=always
 [Install]
 WantedBy=multi-user.target
 EOF
-
-# Set Permissions
-#======#=====#=====
-sudo chown -R prometheus:prometheus /etc/prometheus/
-sudo chmod -R 775 /etc/prometheus/
-sudo chown -R prometheus:prometheus /var/lib/prometheus/
 
 # Reload services file
 #========#=========#====
